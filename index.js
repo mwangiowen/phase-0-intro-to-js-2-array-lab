5ce645fffcb63ffd751f5e097a3836ce9f8cf424
mwangiowennjugunasmith <owen.njugun@student.moringa.com>
2023-09-24 14:17:45 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift
+}
+function appendCat(name) {
+    const newCats = [ ...cats, name];
+    return newCats;
+}function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = [...cats];
+    newCats.pop();
+    return newCats;
+  }
+  function removeFirstCat() {
+    const newCats = [...cats];
+    newCats.shift();
+    return newCats;
+  }
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats; 
+  }
+ 
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  
